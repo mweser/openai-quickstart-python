b37e70459e42f36a51e01f7505b3c9d426158b5d
Matthew Weser <matthew.weser@gmail.com>
2023-01-02 02:21:38 -0800

    Add notes for frequency penalty

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ app = Flask(__name__)
 openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
+# frequency_penalty:
+#
+# Number between -2.0 and 2.0.
+# Positive values penalize new
+# tokens based on their existing
+# frequency in the text so far,
+# decreasing the model's likelihood
+# to repeat the same line verbatim.
+
 @app.route("/", methods=("GET", "POST"))
 def index():
     prompt_to_render = ""
@@ -15,7 +24,7 @@ def index():
         response = openai.Completion.create(
             model="text-davinci-002",
             prompt=in_prompt,
-            temperature=0,
+            temperature=1,
             max_tokens=500,
             top_p=1.0,
             frequency_penalty=0.0,
