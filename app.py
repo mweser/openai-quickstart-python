2b6518eb77f36a68683e64c5f81b28c7dd0bb800
Matthew Weser <matthew.weser@gmail.com>
2023-01-02 01:51:57 -0800

    Clean up output formatting and wrap in box

--- app.py
+++ app.py
@@ -10,16 +10,26 @@ openai.api_key = os.getenv("OPENAI_API_KEY")
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        in_prompt = request.form["prompt_field"]
+        render_template("index.html", prompt_text=in_prompt)
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
-            temperature=0.6,
+            prompt=in_prompt,
+            temperature=0,
+            max_tokens=500,
+            top_p=1.0,
+            frequency_penalty=0.0,
+            presence_penalty=0.0
         )
         return redirect(url_for("index", result=response.choices[0].text))
 
     result = request.args.get("result")
+    print(result)
     return render_template("index.html", result=result)
+
+
+def codegen_prompt(language):
+    return f'Generate a hello world program in {language}'
 
 
 def generate_prompt(animal):
